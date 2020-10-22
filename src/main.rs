0339a7a5d694db7bdf56080b5c187c41bc901868
PRAKASH BHATT <prakashsagwara@gmail.com>
2020-10-22 17:50:52 +0530

    Checkout ownership referencing and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+    let mut s = String::from("New String");
+    update_string(&mut s);
+    // println!("{}", s);
+    let st = &s;
+    // println!("s:{} str:{}", s, st);
+    let data = passing_immutable(st);
+    println!("data is {} \n st is {} and s is {} ", data, st, s);
+}
+
+fn update_string(string: &mut String) {
+    string.push_str(" Updated");
+}
+
+fn passing_immutable(string: &String) -> &String {
+    string
 }
