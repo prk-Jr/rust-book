0e7fa1ee89f95d368e5037f19ffb99a8328c0081
PRAKASH BHATT <prakashsagwara@gmail.com>
2020-10-22 15:23:54 +0530

    This will not work fine if you put print! instead of println in match ordering arms

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
-    println!("Hello, world!");
+    let secret = rand::thread_rng().gen_range(1, 101);
+
+    println!("Hello, world! Secrit is {}", secret);
     println!("This is a guessing game!");
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Some error occured");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Some error occured");
 
-    print!("Number guessed is {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Your guess is less than my secret"),
+            Ordering::Equal => {
+                println!("You got the right answer. Secret is {}", secret);
+                break;
+            }
+            Ordering::Greater => println!("Your guess is more than my secret"),
+        }
+    }
 }
