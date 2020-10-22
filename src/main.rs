fd4d7a470cc7cc22efc8e74d5763b1c2cbb8717a
PRAKASH BHATT <prakashsagwara@gmail.com>
2020-10-22 14:59:31 +0530

    Read from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
 fn main() {
     println!("Hello, world!");
+    println!("This is a guessing game!");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Some error occured");
+
+    print!("Number guessed is {}", guess);
 }
