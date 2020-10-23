acbdb79c290fbd54ae09ce2c6defcddbc9719acf
PRAKASH BHATT <prakashsagwara@gmail.com>
2020-10-23 11:51:24 +0530

    Create a struct with generic type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::ops::Mul;
+
+#[derive(Debug)]
+struct Rectangle<T> {
+    height: T,
+    width: T,
+}
+impl<T> Rectangle<T>
+where
+    T: PartialOrd<T> + Copy + Mul<T, Output = T>,
+{
+    fn new(height: T, width: T) -> Self {
+        Self { height, width }
+    }
+
+    fn area(&self) -> T {
+        self.height * self.width
+    }
+
+    fn is_square(&self) -> bool {
+        self.height == self.width
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let rect = Rectangle::new(50.0, 50.1);
+
+    println!(
+        "Rect is {:?} and area is {:?} and is it square {}",
+        rect,
+        rect.area(),
+        rect.is_square()
+    );
 }
