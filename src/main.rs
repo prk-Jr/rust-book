f93fc0a1d93515dee1bb40e981a2fd47d4a3d029
PRAKASH BHATT <prakashsagwara@gmail.com>
2020-10-25 19:41:23 +0530

    Basic thread creation and understanding how thread is spanned into the programme and then calculated the time required for the same process

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::sync::mpsc;
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let t1 = time::SystemTime::now();
+    let (tx, rx) = mpsc::channel();
+    let (ntx, nrx) = mpsc::channel();
+
+    thread::spawn(move || {
+        thread::sleep(time::Duration::from_millis(3000));
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+    thread::spawn(move || {
+        let val = String::from("hello");
+        ntx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    let receivedn = nrx.recv().unwrap();
+    println!("n Got: {}", receivedn);
+    println!("Got: {}", received);
+    println!(
+        "time since: {:?}",
+        time::SystemTime::now().duration_since(t1)
+    );
 }
