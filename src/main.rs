4ec656f49fe2adfda529a8fc1d9a15a2f86b4802
PRAKASH BHATT <prakashsagwara@gmail.com>
2020-10-25 14:11:58 +0530

    Added changes

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,9 @@ fn main() {
 
 fn call(text: &String, file: &String) {
     println!("Searching for text: {} in file {} ", text, file);
-    let filesys = fs::read_to_string(file)
-        .unwrap_or_else(|res| panic!("Error {:?} occured while reading file: {} ", res, file));
+    let filesys = fs::read_to_string(file).unwrap_or_else(|res| {
+        panic!("Error {:?} occured while reading file: {} ", res, file);
+    });
     for line in search(
         text.to_lowercase().as_str(),
         filesys.to_lowercase().as_str(),
