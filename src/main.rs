6112933948c68d8c68b0050f3828dc312e8caa89
PRAKASH BHATT <prakashsagwara@gmail.com>
2020-10-28 12:40:39 +0530

    Created a basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::*;
 fn main() {
     println!("Hello, world!");
+    let mut count = 0;
+    let server = TcpListener::bind("127.0.0.1:4080").unwrap();
+    for stream in server.incoming() {
+        count += 1;
+        println!("hello {}", count);
+        match stream {
+            Ok(mut data) => {
+                {
+                    thread::spawn(move || handle_stream(&mut data));
+                };
+            }
+            Err(data) => eprint!("Some error occured {:?} at {:?}", data, SystemTime::now()),
+        }
+    }
+}
+
+fn handle_stream(stream: &mut TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut res: Vec<String> = Vec::new();
+    for i in 0..10000 {
+        let var = format!("{}:{}", i, "Hello World");
+        res.push(var);
+    }
+
+    stream
+        .write(format!("{}{:?}", response, res).as_bytes())
+        .unwrap();
+    stream.flush().unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
